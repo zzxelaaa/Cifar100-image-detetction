b24460c7e68adcb02cd6993ec24d3afd0864a78e
zzxelaaa <134096687+zzxelaaa@users.noreply.github.com>
2023-05-20 18:52:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,15 @@ file=st.file_uploader("Choose photo from computer",type=["jpg","png"])
 import cv2
 from PIL import Image,ImageOps
 import numpy as np
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+def import_and_predict(image_data, model):
+    size = (32, 32)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis, ...]
+    img_resized = tf.image.resize(img_reshape, (32, 32))
+    prediction = model.predict(img_resized)
     return prediction
+
 if file is None:
     st.text("Please upload an image file")
 else:
